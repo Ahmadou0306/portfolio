5ee1bea51b0897fa3575340c97b85aa87167cb65
Ahmadou0306 <128220708+Ahmadou0306@users.noreply.github.com>
2024-10-21 17:02:26 +0000

    update text

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-GXJVSG5M.mjs";import"./chunk-T4Q5P63V.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-OOVXPHP5.mjs";import"./chunk-T4Q5P63V.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
